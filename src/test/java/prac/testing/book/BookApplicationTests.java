1bb6380f518cbbcd34d65f253cec14bc158aed5f
Tiago Gomes <tidgomes@gmail.com>
2024-11-13 10:47:54 -0300

    first commit

---
+++ src/test/java/prac/testing/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package prac.testing.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
